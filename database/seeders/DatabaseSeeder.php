d71699fb06b9c7f3c8026668369396875a656a2a
Jose Dominguez Cuero <72531104+JoseDominguezCuero@users.noreply.github.com>
2021-09-25 12:22:26 -0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class UserSeeder extends Seeder
             "name"=>'Jose',
             "email"=>'jdominguez758@misena.edu.co',
             "password" => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',//password
+            "area_id" => '1',
         ]);
     }
 }
